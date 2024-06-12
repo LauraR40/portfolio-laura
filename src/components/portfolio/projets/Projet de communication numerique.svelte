24a4ee962600830711f86efdc96520cf2ca8cc62
Laura <106605150+LauraR40@users.noreply.github.com>
2024-06-12 08:51:31 +0200

    premier projet valide

--- src/components/portfolio/projets/Projet de communication numerique.svelte
+++ src/components/portfolio/projets/Projet de communication numerique.svelte
@@ -1,8 +1,38 @@
-<div>
+<div style="padding: 4rem;">
+  <h1>Gérer un projet de communication numérique</h1>
+  <br />
+  <br />
+  <img
+    style="display: flex; align-items: center;"
+    src="/images/Capture.PNG"
+    alt="apports"
+  />
+  <br />
+  <br />
+  <h2>Contexte & objectifs de résultats</h2>
   <p>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet commodi
-    fuga recusandae autem mollitia alias, quam velit assumenda quod beatae, est
-    dolorem, eius ratione. Quasi eveniet facilis aliquid aspernatur
-    reprehenderit.
+    Ce cahier des charge a été le tout premier que j’ai réalisé, il représente
+    une première approche de la gestion de projet ayant permis une meilleur
+    compréhension des objectifs de communication.
+  </p>
+  <br />
+  <br />
+
+  <h2>Production / travail réalisé</h2>
+  <p>
+    A l’aide d’un document détaillant les exigences de la société ORISUN pour la
+    réalisation de leur projet de documentation ludique, je suis venue récupérer
+    les points clefs nécessaires afin d’organiser et remplir le cahier des
+    charges.
+  </p>
+  <br />
+  <br />
+
+  <h2>Compétences – méthodes :</h2>
+  <p>
+    mobilisées/ développées / acquises Ce travail m’a permis de m’organiser dans
+    ma recherche et d’apprendre à me concentrer sur des points clefs, mais
+    également de comprendre non seulement à quoi sers un cahiers des charge mais
+    aussi la manière de le réaliser.
   </p>
 </div>
