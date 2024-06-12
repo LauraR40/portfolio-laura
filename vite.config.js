81fbecb4bb01ec15746526ee8b41a04e34e37a59
Iziram <github@iziram.fr>
2024-06-12 09:05:00 +0200

    no https for vps

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import path from "path";
-import mkcert from "vite-plugin-mkcert";
 export default defineConfig({
-  plugins: [sveltekit(), mkcert()],
+  plugins: [sveltekit()],
   resolve: {
     alias: {
       $components: path.resolve("./src/components"),
