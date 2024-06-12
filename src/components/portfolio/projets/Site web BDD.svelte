c2ab51d2d5284861e215c8ee5eba0cb52de20a4a
Laura <106605150+LauraR40@users.noreply.github.com>
2024-06-12 08:55:09 +0200

    deux projets

--- src/components/portfolio/projets/Site web BDD.svelte
+++ src/components/portfolio/projets/Site web BDD.svelte
@@ -1,8 +1,38 @@
-<div>
+<div style="padding: 4rem;">
+  <h1>Concevoir un site web avec une source de donnée</h1>
+  <br />
+  <br />
+  <img
+    style="display: flex; align-items: center;"
+    src="/images/Capture.PNG"
+    alt="apports"
+  />
+  <br />
+  <br />
+  <h2>Contexte & objectifs de résultats</h2>
   <p>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet commodi
-    fuga recusandae autem mollitia alias, quam velit assumenda quod beatae, est
-    dolorem, eius ratione. Quasi eveniet facilis aliquid aspernatur
-    reprehenderit.
+    Dans le contexte de cette SAE, par binôme nous devions concevoir un site web
+    relié à une base de donnée. Se devait être le site vitrine d’un magasin d’un
+    magasin de friandise.
+  </p>
+  <br />
+  <br />
+
+  <h2>Production / travail réalisé</h2>
+  <p>
+    -Modélisation des données<br />
+    -Mise en place d’une base de donnée <br />
+    -Réalisation d’un site web avec un back office<br />
+    -Affichage des données grâce à des requêtes SQL
+  </p>
+  <br />
+  <br />
+
+  <h2>Compétences – méthodes :</h2>
+  <p>
+    -Manipulation d’une base de donné<br />
+    -Gestion de session de connexion et création de barre de recherche<br />
+    -Modélisation des données <br />
+    -Génération d’une application web fonctionelle<br />
   </p>
 </div>
